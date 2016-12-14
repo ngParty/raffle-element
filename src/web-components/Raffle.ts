caa7c5008d9fef23e6e8d1575333693600f18c06
Martin Hochel <hochelmartin@gmail.com>
2016-12-14 17:11:24 +0100

    feat(web-component/Raffle): handle raffle winners

--- src/web-components/Raffle.ts
+++ src/web-components/Raffle.ts
@@ -11,12 +11,11 @@ export class Raffle extends HTMLElement {
   static get template() {
     return (`
       <style>${style}</style>
-      <div>Hello custom element</div>
       <form id="${Raffle.refIds.participantsForm}">
         <fieldset class="o-fieldset">
           <div class="o-form-element">
-            <label class="c-label" for="participants">Address line 1:</label>
-            <textarea id="${Raffle.refIds.participants}" class="c-field" placeholder="insert attendes here" rows="5"></textarea>
+            <label class="c-label" for="participants">Add participants</label>
+            <textarea id="${Raffle.refIds.participants}" class="c-field" placeholder="insert attendees here" rows="5"></textarea>
           </div>
           <div class="o-form-element">
             <button class="c-button">Add participants</button>
@@ -24,16 +23,14 @@ export class Raffle extends HTMLElement {
         </fieldset>
       </form>
       <hr>
-      <div class="text-center">
+      <div class="text-center" style="padding-bottom: 8rem">
         <div>            
-          <div id="${Raffle.refIds.participantsCount}" class="c-alert c-alert--info">            
-            42 participants added !
+          <div id="${Raffle.refIds.participantsCount}" class="c-alert c-alert--info" hidden>            
           </div>
           <button id="${Raffle.refIds.startRaffle}" class="c-button c-button--success c-button--block u-super">Get winner</button>
         </div>
         <h2>Winners:</h2>
-        <ul id="${Raffle.refIds.raffleWinners}" class="c-list c-list--unstyled u-super">
-          <li class="c-list__item">Lorem ipsum dolor sit amet</li>         
+        <ul id="${Raffle.refIds.raffleWinners}" class="c-list c-list--unstyled u-super">     
         </ul>  
       </div>
     `)
@@ -56,10 +53,7 @@ export class Raffle extends HTMLElement {
     raffleWinners: HTMLUListElement,
   };
 
-  private state: {
-    participants: string[],
-    winners: string[],
-  };
+  private state: State;
 
   constructor(){
     super();
@@ -74,16 +68,51 @@ export class Raffle extends HTMLElement {
       raffleWinners: shadowRoot.querySelector(`#${Raffle.refIds.raffleWinners}`) as HTMLUListElement,
     };
 
-    this.handleParticipantsAddition = this.handleParticipantsAddition.bind(this);
+    this.state = {
+      participants: [],
+      winners: []
+    };
 
-    this.refs.participantsForm.addEventListener( 'submit', this.handleParticipantsAddition )
+    this.handleParticipantsAddition = this.handleParticipantsAddition.bind(this);
+    this.handleStartRaffleWinner = this.handleStartRaffleWinner.bind(this);
+
+    this.refs.participantsForm.addEventListener( 'submit', this.handleParticipantsAddition );
+    this.refs.startRaffle.addEventListener('click', this.handleStartRaffleWinner);
   }
 
+  private handleStartRaffleWinner( event: Event ) {
+    setTimeout( () => {
+      this.pickWinner();
+      this.render();
+    }, 3000 )
+  }
+
+  private pickWinner(){
+
+    if(!Array.isArray(this.state.participants)){
+      throw new Error('you have to provide participants');
+    }
+    if(!Array.isArray(this.state.winners)){
+      throw new Error('you have to provide winners');
+    }
+
+    const participantsCount = this.state.participants.length;
+    const winner = this.state.participants[ Math.floor( Math.random() * participantsCount ) ];
+    console.log( 'before:', this.state.participants, this.state.winners );
+    this.setState( {
+      winners: [...this.state.winners,winner],
+      participants: this.state.participants.filter( participant => participant !== winner )
+    } );
+    console.log( 'after:', this.state.participants, this.state.winners );
+
+  }
   private handleParticipantsAddition( event: Event ) {
     event.preventDefault();
     const participants = processTextarea( this.refs.participants.value );
     this.setState( { participants } );
     this.refs.participants.value = '';
+
+    this.render();
 
     function processTextarea( value: string ): string[] {
       return value.split( '\n' ).map( participant => participant.trim() ).filter(participant=>participant.length)
@@ -91,7 +120,23 @@ export class Raffle extends HTMLElement {
   }
 
   private setState( newState: State ) {
-    this.state = Object.assign( {}, newState, this.state )
+    this.state = Object.assign( {}, this.state, newState )
+  }
+
+  private render(){
+    this.refs.participantsCount.innerText = `${this.state.participants!.length} participants added !`;
+    this.refs.participantsCount.hidden = false;
+
+    if ( this.state.winners!.length !== this.refs.raffleWinners.childElementCount ) {
+      this.refs.raffleWinners.appendChild( renderWinner( this.state.winners![ this.state.winners!.length - 1 ] ) )
+    }
+
+    function renderWinner( name: string ): HTMLLIElement {
+      const li = document.createElement( 'li' );
+      li.classList.add( 'c-list__item' );
+      li.textContent = name;
+      return li;
+    }
   }
 }
 
