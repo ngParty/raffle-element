8af6392fe04c6eb9d8c2e5eda3fd6848adf2fa82
Martin Hochel <hochelmartin@gmail.com>
2016-12-14 16:17:49 +0100

    feat(web-component/Raffle): use static references for ref points in template and define ref property on element

--- src/web-components/Raffle.ts
+++ src/web-components/Raffle.ts
@@ -7,11 +7,11 @@ export class Raffle extends HTMLElement {
     return (`
       <style>${style}</style>
       <div>Hello custom element</div>
-      <form id="participants-form">
+      <form id="${Raffle.refIds.participantsForm}">
         <fieldset class="o-fieldset">
           <div class="o-form-element">
             <label class="c-label" for="participants">Address line 1:</label>
-            <textarea id="participants" class="c-field" placeholder="insert attendes here" rows="5"></textarea>
+            <textarea id="${Raffle.refIds.participants}" class="c-field" placeholder="insert attendes here" rows="5"></textarea>
           </div>
           <div class="o-form-element">
             <button class="c-button">Add participants</button>
@@ -21,13 +21,13 @@ export class Raffle extends HTMLElement {
       <hr>
       <div class="text-center">
         <div>            
-          <div id="participants-count" class="c-alert c-alert--info">            
+          <div id="${Raffle.refIds.participantsCount}" class="c-alert c-alert--info">            
             42 participants added !
           </div>
-          <button id="start-raffle" class="c-button c-button--success c-button--block u-super">Get winner</button>
+          <button id="${Raffle.refIds.startRaffle}" class="c-button c-button--success c-button--block u-super">Get winner</button>
         </div>
         <h2>Winners:</h2>
-        <ul id="raffle-winners" class="c-list c-list--unstyled u-super">
+        <ul id="${Raffle.refIds.raffleWinners}" class="c-list c-list--unstyled u-super">
           <li class="c-list__item">Lorem ipsum dolor sit amet</li>
           <li class="c-list__item">Consectetur adipiscing elit</li>
           <li class="c-list__item">Nulla volutpat aliquam velit</li>
@@ -35,6 +35,23 @@ export class Raffle extends HTMLElement {
       </div>
     `)
   }
+  static get refIds(){
+    return {
+      participantsForm: 'participants-form',
+      participants: 'participants',
+      participantsCount: 'participants-count',
+      startRaffle: 'start-raffle',
+      raffleWinners: 'raffle-winners',
+    }
+  }
+
+  private refs: {
+    participantsForm: HTMLFormElement,
+    participants: HTMLTextAreaElement,
+    participantsCount: HTMLDivElement,
+    startRaffle: HTMLButtonElement,
+    raffleWinners: HTMLUListElement,
+  };
 
   constructor(){
     super();
